2ac8db52c4ab86520015914a23d1e2041c52f2a7
Mikhail <t9154360684@yandex.ru>
2024-12-09 19:23:08 +0300

    echo "# jamp2" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/makhevich/jamp2.git git push -u origin main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Как-то я подключил гитхаб");
+        String text = "Hello \nworld";
+        System.out.println(text);
     }
 }
