d163ea3eb0aac27c6d6ca2ad945e012b5394f7bd
Mikhail <t9154360684@yandex.ru>
2024-12-15 16:19:11 +0300

    echo "# jamp2" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/makhevich/jamp2.git git push -u origin main

---
+++ src/GuessTheHiddenNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class GuessTheHiddenNumber {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        var attempts = 100;
+
+        var number = ThreadLocalRandom.current().nextInt(0, 100);
+        var currentAttempts = attempts;
+        System.out.println("Выбирите число от 0 до 100 ");
+        while (currentAttempts > 0) {
+            currentAttempts--;
+            var input = Integer.parseInt(scanner.nextLine());
+            if (input == number)
+                break;
+            else
+                System.out.println((input < number ? "Я сам в шоке, но, загаданное число больше, брат" : "Не ожидал от тебя такого. Загаданное число меньше, брат"));
+        }
+        if (currentAttempts > 0)
+            System.out.println("Вы угадали!");
+        else {
+            System.out.println("You loose! Better luck next time!");
+            System.out.println("загаданное число " + number);
+        }
+
+        System.out.println("Количество попыток " + (100 - currentAttempts));
+        System.out.println("загаданное число " + number);
+    }
+}
